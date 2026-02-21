f87d41f045ceed0f4b81764cb7ca56bb53b3a1ab
Inferno Gurala <101462689+infernoGurala@users.noreply.github.com>
2026-02-21 11:56:18 +0530

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+  baseUrl: "utopia.inferalis.space",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
